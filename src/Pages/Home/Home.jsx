1cb31efbfbe0866a3b1829a3f69470af7c9b81e2
sajjadmolliek <sajjadmolliek2018@gmail.com>
2023-10-18 12:46:43 +0600

    Complete Feature Section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import Banner from './Banner/Banner';
+import Feature from './Feature/Feature';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            
+            <Feature></Feature>
         </div>
     );
 };
